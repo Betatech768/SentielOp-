a07ea657aab512eed006ff4113e6251f2797e6d5
Goodness  Onwuanaku <149969859+Betatech768@users.noreply.github.com>
2026-03-16 07:22:14 +0100

    Refactor database connection to use environment variables

--- src/backend/test.py
+++ src/backend/test.py
@@ -1,19 +1,19 @@
 import psycopg2
 import boto3
 load.dotenv()
-password = AWS_PASSWORD
-port = AWS_PORT
-host= AWS_HOST
-username= AWS_USERNAME
-database= AWS_DATABASE
+_user     = os.getenv("DB_USER")
+_password = os.getenv("DB_PASSWORD")
+_host     = os.getenv("DB_HOST")
+_port     = os.getenv("DB_PORT", "5432")
+_name     = os.getenv("DB_NAME")
 conn = None
 try:
     conn = psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=username,
-        password=password,
+        host=_host,
+        port=_port,
+        database=_name,
+        user=_user,
+        password=_password,
         sslmode='verify-full',
     sslrootcert='/certs/global-bundle.pem'
     )
